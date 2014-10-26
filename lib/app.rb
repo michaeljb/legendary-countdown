c8b7413005712e80d80943b73ac9c7f67d631dd5
Michael Brandt <mbrandt@colorado.edu>
2014-10-26 08:23:45 -0600

    Move everything into lib/.

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
 require 'yaml'
 
-opts = YAML.load_file('conf/options.yaml')
+opts = YAML.load_file(
+    File.join(File.dirname(__FILE__), 'conf', 'options.yaml')
+  )
 
 get '/' do
   erb :index, locals: opts
