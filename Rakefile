afde467a838dc04758ef6f6b79cd8127e4c82875
Michael Brandt <mbrandt@colorado.edu>
2014-10-26 13:17:42 -0600

    Add rake task to start server, modularize the app.
    - start local server with `rake server:start`
    
    - rename YAML file with all of the game setup data
    
    - put the app inside of a class

--- Rakefile
+++ Rakefile
@@ -5,3 +5,10 @@ require 'cucumber/rake/task'
 Cucumber::Rake::Task.new(:features) do |t|
   t.cucumber_opts = "features --format pretty"
 end
+
+namespace :server do
+  desc 'Start the Sinatra server on port 4567'
+  task :start do
+    sh "ruby lib/app.rb"
+  end
+end
--- lib/app.rb
+++ lib/app.rb
@@ -1,14 +1,20 @@
 require 'sinatra'
+require 'sinatra/base'
 require 'yaml'
 
-opts = YAML.load_file(
-    File.join(File.dirname(__FILE__), 'conf', 'options.yaml')
-  )
+class LegendaryCountdown < Sinatra::Application
+  set :setup_choices, YAML.load_file(
+      File.join(File.dirname(__FILE__), 'conf', 'setup_choices.yaml')
+    )
 
-get '/' do
-  erb :index, locals: opts
-end
+  get '/' do
+    erb :index, locals: settings.setup_choices
+  end
 
-post '/submit' do
-  "Your Mastermind was #{params['mastermind']}"
+  post '/submit' do
+    "Your Mastermind was #{params['mastermind']}"
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
