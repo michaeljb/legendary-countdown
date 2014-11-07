1128fc4e7fa5167cc060ffd929d9f6517010a5ec
Michael Brandt <mbrandt@colorado.edu>
2014-11-06 20:50:51 -0700

    Implement Steal the Weaponized Plutonium; all pass
    This scheme has the same turn expectancy as Save Humanity because it
    adds a Villain Group (+8 cards), but each Twist plays another card from
    the Villain Deck (-8 cards) for a net change of 0 cards.

--- lib/schemes/steal_the_weaponized_plutonium.rb
+++ lib/schemes/steal_the_weaponized_plutonium.rb
@@ -1,6 +1,11 @@
-# Setup: 8 Twists representing Plutonium. Add an extra Villain Group.
 class StealTheWeaponizedPlutonium < Scheme
+  # Setup: ... Add an extra Villain Group.
   def villains
     super + 8
+  end
+
+  # Twist: ... Either way, play another card from the Villain Deck.
+  def plays_more_villain_cards
+    1
   end
 end
