2dcaf5735b67ee5056f1a72a95e8e65ddfca60b0
Michael Brandt <mbrandt@colorado.edu>
2014-11-07 21:36:00 -0700

    Don't display setup for cards with 0 count.
    Some Schemes have no Bystanders in Villain Deck, so for setup don't
    bother displaying as "0 Bystanders". This also works better for Replace
    Earth's Leaders with Killbots, since we mention Bystanders in the
    Killbots parens.

--- features/step_definitions/contents_steps.rb
+++ features/step_definitions/contents_steps.rb
@@ -1,7 +1,9 @@
 Then(/^the Villain Deck should contain (\d+) (.*)$/) do |count, card|
   card.chomp!('s') if count.to_i == 1 && card.end_with?('s')
 
-  expect(find('.result')).to have_content("#{count} #{card}") if count != ''
+  if count.to_i != 0
+    expect(find('.result')).to have_content("#{count} #{card}")
+  end
 end
 
 # if the other_count and other_type columns are blank, the step matches this
