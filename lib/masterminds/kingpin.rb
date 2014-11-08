9b19ee04a9c8c2b321b504cdc79fe970dd984a8b
Michael Brandt <mbrandt@colorado.edu>
2014-11-07 20:12:34 -0700

    Fix Kingpin implementation.

--- lib/masterminds/kingpin.rb
+++ lib/masterminds/kingpin.rb
@@ -3,7 +3,7 @@ class Kingpin < Mastermind
     {
       # Criminal Empire has Fight: If this is not the final Tactic, reveal the
       # top three cards of the Villain Deck. Play all the Villains you revealed.
-      any: 3
+      villain: 3
     }
   end
 end
