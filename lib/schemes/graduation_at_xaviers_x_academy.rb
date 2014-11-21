6e9706bee655ff4dcb299bdc00f9350d3c967034
Michael Brandt <mbrandt@colorado.edu>
2014-11-20 20:40:31 -0700

    Implement Plot Graduation at Xavier's X-Academy
    While *technically* this Plot doesn't have a "Good Wins" condition on
    Twist 8, if no Bystanders are carried to safety there will still be 8 in
    the overrun pile on Twist 8 as each Twist puts one there.
    
    Fixes 3 tests.
    
    [GH-15]

---
+++ lib/schemes/graduation_at_xaviers_x_academy.rb
@@ -0,0 +1,8 @@
+# Setup: 8 Twists. Stack 8 Bystanders next to this Plot as "Young Mutants."
+# Twist: Put a Bystander from next to this Plot into the Overrun Pile.
+# Good Wins: When there are 8 Bystanders in the Overrun Pile.
+class GraduationAtXaviersXAcademy < Scheme
+  def evil_wins_on_twist
+    8
+  end
+end
