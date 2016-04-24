e7bf5627d59749c4b3135e10f5a2305c0d2fbddb
Michael Brandt <michaelbrandt5@gmail.com>
2016-04-24 16:34:21 -0600

    Better spacing in store/index.js

--- app/js/store/index.js
+++ app/js/store/index.js
@@ -39,64 +39,68 @@ const defaultState = {
 
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
-  case 'SET_MODE':
-    return {
-      ...state,
-      mode: action.text
-    };
-  case 'ADD_MASTERMIND':
-    return {
-      ...state,
-      masterminds: [
-        ...state.masterminds,
-        newMastermind()
-      ]
-    }
-  case 'REMOVE_MASTERMIND':
-    if (state.masterminds.length === 1) {
-      return state;
-    }
+    case 'SET_MODE':
+      return {
+        ...state,
+        mode: action.text
+      };
 
-    return {
-      ...state,
-      masterminds: state.masterminds.slice(0, -1)
-    }
-  case 'ADD_VILLAIN_GROUP':
-    return {
-      ...state,
-      villainGroups: [
-        ...state.villainGroups,
-        newVillainGroup()
-      ]
-    }
-  case 'REMOVE_VILLAIN_GROUP':
-    if (state.villainGroups.length === 1) {
-      return state;
-    }
+    case 'ADD_MASTERMIND':
+      return {
+        ...state,
+        masterminds: [
+          ...state.masterminds,
+          newMastermind()
+        ]
+      }
+    case 'REMOVE_MASTERMIND':
+      if (state.masterminds.length === 1) {
+        return state;
+      }
 
-    return {
-      ...state,
-      villainGroups: state.villainGroups.slice(0, -1)
-    }
-  case 'ADD_HENCHMEN_GROUP':
-    return {
-      ...state,
-      henchmenGroups: [
-        ...state.henchmenGroups,
-        newHenchmenGroup()
-      ]
-    }
-  case 'REMOVE_HENCHMEN_GROUP':
-    if (state.henchmenGroups.length === 1) {
-      return state;
-    }
+      return {
+        ...state,
+        masterminds: state.masterminds.slice(0, -1)
+      }
 
-    return {
-      ...state,
-      henchmenGroups: state.henchmenGroups.slice(0, -1)
-    }
-  default:
-    return state;
+    case 'ADD_VILLAIN_GROUP':
+      return {
+        ...state,
+        villainGroups: [
+          ...state.villainGroups,
+          newVillainGroup()
+        ]
+      }
+    case 'REMOVE_VILLAIN_GROUP':
+      if (state.villainGroups.length === 1) {
+        return state;
+      }
+
+      return {
+        ...state,
+        villainGroups: state.villainGroups.slice(0, -1)
+      }
+
+    case 'ADD_HENCHMEN_GROUP':
+      return {
+        ...state,
+        henchmenGroups: [
+          ...state.henchmenGroups,
+          newHenchmenGroup()
+        ]
+      }
+    case 'REMOVE_HENCHMEN_GROUP':
+      if (state.henchmenGroups.length === 1) {
+        return state;
+      }
+
+      return {
+        ...state,
+        henchmenGroups: state.henchmenGroups.slice(0, -1)
+      }
+
+    default:
+      return state;
   }
 };
 
