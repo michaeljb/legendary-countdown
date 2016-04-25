8fe18496037df4de29c9e0255d6fb247ee006f49
Michael Brandt <michaelbrandt5@gmail.com>
2016-04-25 17:34:30 -0600

    Fix eslint errors

--- app/js/store/index.js
+++ app/js/store/index.js
@@ -39,81 +39,81 @@ const defaultState = {
 
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
-    case 'SET_MODE':
-      return {
-        ...state,
-        mode: action.text
-      };
+  case 'SET_MODE':
+    return {
+      ...state,
+      mode: action.text
+    };
 
-    case 'ADD_MASTERMIND':
-      return {
-        ...state,
-        masterminds: [
-          ...state.masterminds,
-          newMastermind(mastermindID++)
-        ]
-      }
-    case 'REMOVE_MASTERMIND':
-      if (state.masterminds.length === 1) {
-        return state;
-      }
-
-      return {
-        ...state,
-        masterminds: state.masterminds.slice(0, -1)
-      }
-    case 'SET_MASTERMIND':
-      var index;
-      for (var i = 0; i < state.masterminds.length; i++) {
-        if (state.masterminds[i].id === action.id) {
-          index = i;
-        }
-      }
-      var masterminds = state.masterminds.slice();
-      masterminds[index] = newMastermind(action.id, action.name);
-      return {
-        ...state,
-        masterminds
-      }
-
-    case 'ADD_VILLAIN_GROUP':
-      return {
-        ...state,
-        villainGroups: [
-          ...state.villainGroups,
-          newVillainGroup()
-        ]
-      }
-    case 'REMOVE_VILLAIN_GROUP':
-      if (state.villainGroups.length === 1) {
-        return state;
-      }
-
-      return {
-        ...state,
-        villainGroups: state.villainGroups.slice(0, -1)
-      }
-
-    case 'ADD_HENCHMEN_GROUP':
-      return {
-        ...state,
-        henchmenGroups: [
-          ...state.henchmenGroups,
-          newHenchmenGroup()
-        ]
-      }
-    case 'REMOVE_HENCHMEN_GROUP':
-      if (state.henchmenGroups.length === 1) {
-        return state;
-      }
-
-      return {
-        ...state,
-        henchmenGroups: state.henchmenGroups.slice(0, -1)
-      }
-
-    default:
+  case 'ADD_MASTERMIND':
+    return {
+      ...state,
+      masterminds: [
+        ...state.masterminds,
+        newMastermind(mastermindID++)
+      ]
+    }
+  case 'REMOVE_MASTERMIND':
+    if (state.masterminds.length === 1) {
       return state;
+    }
+
+    return {
+      ...state,
+      masterminds: state.masterminds.slice(0, -1)
+    }
+  case 'SET_MASTERMIND':
+    var index;
+    for (var i = 0; i < state.masterminds.length; i++) {
+      if (state.masterminds[i].id === action.id) {
+        index = i;
+      }
+    }
+    var masterminds = state.masterminds.slice();
+    masterminds[index] = newMastermind(action.id, action.name);
+    return {
+      ...state,
+      masterminds
+    }
+
+  case 'ADD_VILLAIN_GROUP':
+    return {
+      ...state,
+      villainGroups: [
+        ...state.villainGroups,
+        newVillainGroup()
+      ]
+    }
+  case 'REMOVE_VILLAIN_GROUP':
+    if (state.villainGroups.length === 1) {
+      return state;
+    }
+
+    return {
+      ...state,
+      villainGroups: state.villainGroups.slice(0, -1)
+    }
+
+  case 'ADD_HENCHMEN_GROUP':
+    return {
+      ...state,
+      henchmenGroups: [
+        ...state.henchmenGroups,
+        newHenchmenGroup()
+      ]
+    }
+  case 'REMOVE_HENCHMEN_GROUP':
+    if (state.henchmenGroups.length === 1) {
+      return state;
+    }
+
+    return {
+      ...state,
+      henchmenGroups: state.henchmenGroups.slice(0, -1)
+    }
+
+  default:
+    return state;
   }
 };
 
