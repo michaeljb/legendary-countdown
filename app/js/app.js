8f863eeac995d427b48f704ece3ce3966ff8e69b
Michael Brandt <michaelbrandt5@gmail.com>
2016-04-30 17:46:57 -0600

    Add in SimpleSelect element from react-selectize

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom';
 
 import AppRoot from './components/AppRoot.jsx';
 
+import '!style!css!react-selectize/dist/index.css';
+
 ReactDOM.render(
   AppRoot,
   document.getElementById('app-root')
--- app/js/components/AppRoot.jsx
+++ app/js/components/AppRoot.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {SimpleSelect} from 'react-selectize';
 
 import Mode from './Mode.jsx';
 import Scheme from './Scheme.jsx';
@@ -14,8 +15,20 @@ const logit = () => console.log(store.getState().toJS());
 store.subscribe(logit);
 logit();
 
+//return <SimpleSelect options = {[{label: '2p', value: '2p'}]} placeholder = "Mode" />
+
+const Form = React.createClass({
+  render: () => {
+    const options = ['apple', 'mango', 'grapes', 'melon', 'strawberry'].map(function(fruit){
+      return {label: fruit, value: fruit}
+    });
+    return <SimpleSelect options = {options} placeholder = 'Select a fruit'></SimpleSelect>
+  }
+});
+
 const App = () => (
   <div>
+    <Form />
     <Mode store={store} />
     <Scheme store={store} />
     <Masterminds />
