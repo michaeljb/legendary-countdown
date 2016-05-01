8b9eb2113dc1614a865a035424fdf9b0bc9768c7
Michael Brandt <michaelbrandt5@gmail.com>
2016-04-30 22:11:25 -0600

    Code cleanup in Selector

--- app/js/components/Selector.jsx
+++ app/js/components/Selector.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-
 import {SimpleSelect} from 'react-selectize';
 
 const Selector = (props) => {
   const {theState, placeholder, optionList, onValueChange} = props;
 
-  const options = optionList.map((opt) => {return {label: opt, value: opt}});
+  const options = optionList.map((opt) => ({label: opt, value: opt}));
 
-  const theStateOption = {label: theState, value: theState}
-
+  const theStateOption = {label: theState, value: theState};
   const value = typeof theState === 'undefined' ? null : theStateOption;
 
   return (
